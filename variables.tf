5970c3d667abac9c05bd3afe4a062342f6320a2c
bassbone <bassbone0o_o0@yahoo.co.jp>
2021-01-12 03:09:34 +0000

    add description

--- variables.tf
+++ variables.tf
@@ -1,20 +1,25 @@
 variable "zone" {
+  description = "zone with the instance"
   default = "us-central1-b"
 }
 
 variable "instance_start" {
+  description = "flag to start instance"
   default = false
 }
 
 variable "instance_stop" {
+  description = "flag to stop instance"
   default = true
 }
 
 variable "schedule_start" {
+  description = "schedule to start"
   default = "0 9 * * *"
 }
 
 variable "schedule_stop" {
+  description = "schedule to stop"
   default = "0 3 * * *"
 }
 
